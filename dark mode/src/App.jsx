72acab052a6093d7dc32193e61ebadc3d9d8df36
RajeevPatel <rajeevpatel8985@gmail.com>
2024-05-07 10:55:15 +0530

    Dark Mode : here state will manage by using useContext

---
+++ dark mode/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ThemeBtn from "./Components/ThemeBtn";
+import { ThemeProvider } from "./Context/Theme";
+import { useState } from "react";
+import { useEffect } from "react";
+import Card from "./Components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+  return (
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
