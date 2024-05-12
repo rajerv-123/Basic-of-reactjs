7845e0338d2093cf4600e3b6d2b22133bbe590eb
RajeevPatel <rajeevpatel8985@gmail.com>
2024-05-12 07:24:14 +0530

    changes resolved

---
+++ 06 React Redux/reaact-redux/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "./Components/Todo";
+import AddTodo from "./Components/AddTodo";
+function App() {
+  return (
+    <div>
+      <AddTodo />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
